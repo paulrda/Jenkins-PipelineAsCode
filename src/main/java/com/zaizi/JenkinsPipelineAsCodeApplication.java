5f0c9cc57ab93419da05f5805cf0f35752fe75f8
Ronald Alles <ralles@zaizi.com>
2016-11-17 15:15:49 +0530

    chenaged log 3

--- src/main/java/com/zaizi/JenkinsPipelineAsCodeApplication.java
+++ src/main/java/com/zaizi/JenkinsPipelineAsCodeApplication.java
@@ -8,5 +8,7 @@ public class JenkinsPipelineAsCodeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsPipelineAsCodeApplication.class, args);
+		
+		System.out.println("**** Testing Hellow World");
 	}
 }
