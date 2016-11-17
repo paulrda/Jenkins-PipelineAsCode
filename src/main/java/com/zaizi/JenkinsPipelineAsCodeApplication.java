c8895816595c210073b768e1a37f83906b1d49a8
Ronald Alles <ralles@zaizi.com>
2016-11-17 14:40:41 +0530

    Added All files

---
+++ src/main/java/com/zaizi/JenkinsPipelineAsCodeApplication.java
@@ -0,0 +1,12 @@
+package com.zaizi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsPipelineAsCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsPipelineAsCodeApplication.class, args);
+	}
+}
